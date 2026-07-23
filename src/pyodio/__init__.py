5870db3e5e4c5130ae94f335d8d1302c0f7545c8
Mathieu Réquillart <mathieu.requillart@gmail.com>
2026-07-23 20:20:38 +0200

    bump 0.1.0

--- src/pyodio/__init__.py
+++ src/pyodio/__init__.py
@@ -63,7 +63,7 @@ from .models import (
 from .rest import DEFAULT_BASE_URL, OdioClient
 from .sse import EventStream, OdioEvent, stream_events
 
-__version__ = "0.1.0rc3"
+__version__ = "0.1.0"
 
 __all__ = [
     "ADDED",
